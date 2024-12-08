77bd15ce5df382dedae595a562a625b0bde98e31
Brad <brad@4pi.com.au>
2024-12-08 19:45:06 +1100

    Initial commit

---
+++ MauiIssueMacCatalystButtonText/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiIssueMacCatalystButtonText;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ MauiIssueMacCatalystButtonText/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiIssueMacCatalystButtonText;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
